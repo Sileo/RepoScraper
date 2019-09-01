02d7cd59009357e6ba15cb26dd89af4115a90481
KhafraDev <42794878+KhafraDev@users.noreply.github.com>
2019-09-01 14:17:53 -0400

    wrong file uploaded

--- Rpetrich.js
+++ Rpetrich.js
@@ -7,30 +7,47 @@
 	let changelogStackView = SileoGen.generateStackView();
 	changelogStackView.tabname = "Changelog"
 
-	panels.forEach(function(panel){
-		let stackView = detailsStackView;
+	let holder = [];
+	let cHolder = [];
 
+	panels.forEach(function(panel){
 		let fieldsets = panel.getElementsWithTag("fieldset");
-		fieldsets.forEach(function(fieldset){
-			//let tables = fieldset.getElementsWithTag("table");
+
+		fieldsets.forEach(function(fieldset, ind){
 			let elements = fieldset.children();
+
 			elements.forEach(function(el, i) {
-				detailsStackView.views.push('KHAFRA!!!')
+				if(el.tag() === 'div' && el.children().length && el.children()[0].tag() === 'p') {
+					if(fieldset.attr('style')) {
+						holder.push(el.children()[0].text());
+					} else {
+						cHolder.push(el.text());
+					}
+				} else if(el.tag() === 'a') {
+					let href = el.attr('href'); // thx CS
+					detailsStackView.views.push(SileoGen.generateTableButton(el.text(), absoluteURL(href)))
+				}
 			});
 		});
-
-		stackView.views.push(SileoGen.generateSeparator());
 	});
 
-	if(changelogStackView.views.length === 0) {
-		changelogStackView.views.push(SileoGen.generateMarkdown('No changelogs are available for this package!')); 
-	}
+	/*
+	https://stackoverflow.com/a/14438954
+	In newer syntax I would just use [...new Set(arr)], but a filter will work!
+	*/
+	holder.filter(function(value, index, self) { 
+		return self.indexOf(value) === index;
+	}).forEach(function(i) {
+		detailsStackView.views.push(SileoGen.generateMarkdown(i));
+	});
 
-	let origButton = SileoGen.generateTableButton("View Original Depiction", ".");
-	detailsStackView.views.push(origButton);
+	cHolder.filter(function(value, index, self) { 
+		return self.indexOf(value) === index;
+	}).forEach(function(i) {
+		changelogStackView.views.push(SileoGen.generateMarkdown(i));
+	});
 
 	let disclaimer = SileoGen.generateMarkdown("<span>This depiction has been automatically generated. It may be missing information.</span>");
-	disclaimer.useRawFormat = true;
 	detailsStackView.views.push(disclaimer);
 
 	let rootView = {
